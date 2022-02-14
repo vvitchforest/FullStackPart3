660ba48b00c82309f85c15d5dfe4cb93320c2f16
irina <irina.konovalova455@gmail.com>
2022-02-14 19:37:14 +0200

    3.22 tehty

--- models/person.js
+++ models/person.js
@@ -1,42 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 3,
-    required: [true, "Name is required"],
+    required: [true, 'Name is required'],
   },
   number: {
     type: String,
     minlength: 8,
-    required: [true, "Phone number required"],
+    required: [true, 'Phone number required'],
     validate: {
       validator: (v) => {
-        return /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})/.test(v);
+        return /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number`,
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,41 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://irinakon:${password}@cluster0.mfet4.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+const url = `mongodb+srv://irinakon:${password}@cluster0.mfet4.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (name && number) {
   const person = new Person({
     name: name,
     number: number,
-  });
-  person.save().then((result) => {
-    console.log(`added ${person.name} number ${person.number} to phonebook`);
-    mongoose.connection.close();
-  });
+  })
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
   Person.find({}).then((result) => {
-    console.log("phonebook: ")
+    console.log('phonebook: ')
     result.forEach((person) => {
-      console.log(person.name, person.number);
-    });
-    mongoose.connection.close();
-  });
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
 }
 
