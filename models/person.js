f07603cd5699f86979906790a017b2661d78df71
irina <irina.konovalova455@gmail.com>
2022-02-14 15:42:58 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const personSchema = new mongoose.Schema({
     required: [true, "Phone number required"],
     validate: {
       validator: (v) => {
-        return /^\d{2,3}-\d{6,}/.test(v);
+        return /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number`,
     },
